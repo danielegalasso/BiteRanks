01752cd64c84b14b0f103e9bdbc0e27b90422a69
domenico3622 <128099390+domenico3622@users.noreply.github.com>
2024-09-12 22:20:43 +0200

    prova App.jsx merge con domenico
    aggiunto per priva la route /map

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -11,48 +11,10 @@ import TextPlusIcon from "./home/TextPlusIcon";
 
 // Dati dei marker
 export default function App() {
-
   const [selectedItems, setSelectedItems] = useState([]); // Stato per gli elementi selezionati
-
-  const nomeLocale = "Panificio Menchetti";
-  const classifiche = [
-    ["Top Pizza in Viaggio in Italia", 25, "https://example.com/top-pizza"],
-    ["Tre Spicchi Gambero Rosso", 3, "https://example.com/gambero-rosso"]
-  ];
-  const linkGoogleMaps = "https://maps.google.com/?q=Panificio+Menchetti";
-  const linkIndicazioniMaps = "https://maps.google.com/dir/?api=1&destination=Panificio+Menchetti";
-  const linkSitoWeb = "https://www.panificiomenchetti.it";
-  const locations = [
-    {
-      nome: 'Luogo 1',
-      coordinates: [51.505, -0.09],
-      classifiche: [['Classifica 1', 1, 'https://link1.com']],
-      linkGoogleMaps: 'https://maps.google.com',
-      linkIndicazioniMaps: 'https://maps.google.com/dir',
-      linkSitoWeb: 'https://example.com'
-    },
-    {
-      nome: 'Luogo 2',
-      coordinates: [51.515, -0.1],
-      classifiche: [['Classifica 2', 2, 'https://link2.com']],
-      linkGoogleMaps: 'https://maps.google.com',
-      linkIndicazioniMaps: 'https://maps.google.com/dir',
-      linkSitoWeb: 'https://example.com'
-    }
-  ];
-
-
   // State to hold markers data
   const [markers, setMarkers] = useState([]);
-
-
   const [isFoodSearchVisible, setFoodSearchVisible] = useState(true);
-
-
-
-
-  
-
   const emptyMarkers = []; 
 
   return (
@@ -66,11 +28,13 @@ export default function App() {
 
           </>
         }/>
-
-
-      
-      
-
+        {/* AGGIUNTA DA ME /map NON CI DOVREBBE ESSERE*/ }
+        <Route path="/map" element={
+          <>         
+          <Map markers={markers} />
+          <SearchBarWithAutocomplete />
+          </>
+        }/> 
       </Routes>
     </Router>
   );
