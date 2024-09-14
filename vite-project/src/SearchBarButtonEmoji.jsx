0a1ca629d763cbfd1f35acc2b768570111695084
Daniele Galasso <64840503+danielegalasso@users.noreply.github.com>
2024-09-14 17:39:27 +0200

    Update SearchBarButtonEmoji.jsx

--- vite-project/src/SearchBarButtonEmoji.jsx
+++ vite-project/src/SearchBarButtonEmoji.jsx
@@ -3,15 +3,21 @@ import closeicon from './home/emoji/closeicon.png'
 import './SearchBarButtonEmoji.css'
 
 
-const ButtonEmoji = ({ iconSrc, onClick}) => {
+
+const ButtonEmoji = ({ iconSrc, onClick }) => {
+  const color = "blue";
+
   return (
-    <button className="custom-button" onClick={onClick} >
-
-      <img src={iconSrc} alt="close" className="src-icon" />
-      <img src={closeicon} alt="close" className="x-icon"  />
-
+    <button
+      className="custom-button"
+      onClick={onClick}
+      style={{ border: `0.15rem solid ${color}` }}
+    >
+      <img src={iconSrc} alt="icon" className="src-icon" />
+      <img src={closeicon} alt="close" className="x-icon" />
     </button>
   );
 };
 
 export default ButtonEmoji;
+
