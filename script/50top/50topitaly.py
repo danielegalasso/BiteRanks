51f4fd2e9a89a6742c585f900fb7622561c2f9d2
Daniele Galasso <64840503+danielegalasso@users.noreply.github.com>
2024-11-09 22:48:38 +0100

    50 Top Italy + Birre 2025

--- script/50top/50topitaly.py
+++ script/50top/50topitaly.py
@@ -1,9 +1,11 @@
+# updated script to new json v1.2
+
 import re
 import requests
 from bs4 import BeautifulSoup
-from time import sleep
 import json
-
+from time import sleep
+import os
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
@@ -12,24 +14,24 @@ headers = {
 category = '🍽️'
 
 urls = [
-    'https://www.50topitaly.it/it/i-migliori-ristoranti-italiani-2024-grandi-ristoranti/',
-    'https://www.50topitaly.it/it/i-migliori-ristoranti-italiani-2024-cucina-dautore/',
-    'https://www.50topitaly.it/it/50-top-trattorie-bistro-moderni-2024/',
-    'https://www.50topitaly.it/it/50-top-italy-migliori-sushi-2024/',
-    'https://www.50topitaly.it/it/50-top-italy-migliori-bistro-vegetali-2024/',
-    'https://www.50topitaly.it/it/50-top-italy-migliori-cacio-e-pepe-2024/',
-    'https://www.50topitaly.it/it/50-top-italy-migliori-panini-2024/',
-    'https://www.50topitaly.it/it/50-top-italy-migliori-street-food-2024/',
-    'https://www.50topitaly.it/it/50-top-italy-migliori-ristoranti-di-pesce-2024/',
-    'https://www.50topitaly.it/it/i-migliori-ristoranti-italiani-nel-mondo-2024/'
+
+    'https://www.50topitaly.it/it/i-migliori-panini-2025-mulino-caputo-award/',
+    "https://www.50topitaly.it/it/i-migliori-street-food-2025-il-fritturista-oleificio-zucchi-award/",
+    "https://www.50topitaly.it/it/le-migliori-amatriciane-2025-ferrarelle-award/",
+    "https://www.50topitaly.it/it/i-migliori-ristoranti-sotto-i-100e-2025-cremoso-la-dispensa-award/",
+    "https://www.50topitaly.it/it/50-top-trattorie-bistro-moderni-2025/",
+
+
 ]
+
+#urls = ['https://www.50topitaly.it/it/i-migliori-panini-2025-mulino-caputo-award/',       
+#    ]
 
 def extract_name_from_url(url):
     path = url.split('/')[-2]
     name = path.replace('-', ' ').title()
     name = re.sub(r'[^\w\s]', '', name)
     return name
-
 
 # PRIMA PARTE: ESTRARRE I RIFERIMENTI
 url_to_name = {url: extract_name_from_url(url) for url in urls}
@@ -46,22 +48,55 @@ for url in urls:
         position = card.find('h2', class_='posizione scotchmodern rosso caps')
         position = None if not position else position.get_text(strip=True)
         place_name = card.find('h3', class_='titolo roboto nero caps')
-        url_to_reference[url].append({'category': category ,'position': position, 'name': place_name.get_text(strip=True), 'ref': card.get('href'), 'address': [], 'coord': [], 'website': None})
-
+        url_to_reference[url].append({
+            'ranking': '50 Top Italy',
+            'sub-ranking': extract_name_from_url(url),
+            'emoji': category,
+            'position': position,
+            'name': place_name.get_text(strip=True),
+            'ref': card.get('href'),
+            'address': [],
+            'coord': [],
+            'website': None
+        })
 
 # SECONDA PARTE: DERIVARE LA POSIZIONE
+def fetch_with_retry(url, headers, retries=3):
+    for i in range(retries):
+        try:
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()
+            return response
+        except requests.exceptions.RequestException as e:
+            if i < retries - 1:
+                print(f"Errore di connessione, tentativo {i + 1} di {retries}... ({e})")
+                sleep(5)
+            else:
+                raise
+
 for url in url_to_reference:
     for data in url_to_reference[url]:
         print(data['ref'])
-        response = requests.get(data['ref'], headers=headers)
-        response.raise_for_status()
+        response = fetch_with_retry(data['ref'], headers)
         soup = BeautifulSoup(response.text, 'html.parser')
+
+        website_div = next((div for div in soup.find_all('div') if re.search(r'^sito web:.*', div.get_text(strip=True))), None)
+        if website_div is not None:
+            website = website_div.find('a')
+            data['website'] = website.get('href')
 
         locations = soup.find_all('a', href=lambda href: href and href.startswith('https://www.google.com/maps/dir/?api'))
         for location in locations:
-
-            address = location.find('span')
-            data['address'].append(address.get_text(strip=True))
+            raw_address = location.get_text(strip=True)
+            try:
+                raw_address = raw_address.split('indirizzo: ')[1]
+            except IndexError as e:
+                pass
+            finally:
+                address = raw_address
+                #print(address)
+                #print(address.replace("localizzatore", ""))
+                data['address'].append(address.replace("localizzatore", ""))
 
             href = location.get('href')
             if 'destination=' in href:
@@ -77,13 +112,13 @@ for url in url_to_reference:
                         print(f"Errore nella conversione delle coordinate da {data['ref']}: {lat_long}")
         sleep(1)
 
+# Salvare il file JSON nella directory corrente
 for url, name in url_to_name.items():
     json_output = {}
     json_output[name] = url_to_reference[url]
-    with open(f'vite-project/public/ranking/50TopItaly/{name.replace(" ", "")}.json', 'w', encoding='utf-8') as file:
+    output_path = f'{name.replace(" ", "")}.json'
+    with open(output_path, 'w', encoding='utf-8') as file:
         json.dump(json_output, file, ensure_ascii=False, indent=4)
 
-'''
-with open('50topitaly.json', 'w', encoding='utf-8') as file:
-    json.dump(json_output, file, ensure_ascii=False, indent=4)
-'''
+''' with open('50topitaly.json', 'w', encoding='utf-8') as file:
+    json.dump(json_output, file, ensure_ascii=False, indent=4) '''
